ecfd2b965a905a5a7015c03a01728924f6b9865f
Andrew Kane <andrew@ankane.org>
2022-09-05 13:32:25 -0700

    Added logging for tests with redis 5 gem [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,16 @@ $logger = Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
 $logger.formatter = ->(severity, datetime, progname, msg) { "#{msg}\n" }
 
 Dalli.logger = $logger
+
+module RedisInstrumentation
+  def call(command, redis_config)
+    $logger.info(command.inspect)
+    super
+  end
+
+  def call_pipelined(commands, redis_config)
+    $logger.info(commands.inspect)
+    super
+  end
+end
+RedisClient.register(RedisInstrumentation) if defined?(RedisClient)
